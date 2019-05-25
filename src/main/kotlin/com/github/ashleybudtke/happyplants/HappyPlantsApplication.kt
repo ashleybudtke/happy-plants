79a1d6f0a89c6a105e9611e64f86c59d3a343a46
Ashley Budtke <ashleybudtke@gmail.com>
2019-05-25 15:10:29 -0500

    Initial commit: Kotlin/Gradle hello world

---
+++ src/main/kotlin/com/github/ashleybudtke/happyplants/HappyPlantsApplication.kt
@@ -0,0 +1,12 @@
+package com.github.ashleybudtke.happyplants
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HappyPlantsApplication
+
+fun main(args: Array<String>) {
+	runApplication<HappyPlantsApplication>(*args)
+	println("Hello World!")
+}
